ad7a8482973014ab17ad9ce058230da5d3fa91d1
Sunil Rehman <sunil.rehman@gmail.com>
2021-09-30 12:55:07 -0700

    Version updated 2021.09.30

--- Cards56Web/wwwroot/table.js
+++ Cards56Web/wwwroot/table.js
@@ -15,7 +15,7 @@ class Table
     this._init_koolies();
     this._init_scores();
     this._init_center_display();
-    this._init_version("2021.09.23");
+    this._init_version("2021.09.30");
     this._init_alert_message();
     this._init_table_buttons();
   }
