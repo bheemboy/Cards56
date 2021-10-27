98514384707a47757c1645ae6a4d0b6a53c6a369
Sunil Rehman <sunil.rehman@gmail.com>
2021-10-27 08:48:02 -0700

    Serving static pages from nginx

--- Cards56Web/Startup.cs
+++ Cards56Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace Cards56Web
 {
@@ -20,11 +21,16 @@ namespace Cards56Web
         {
             services.AddCors();
             services.AddSignalR();
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -38,12 +44,12 @@ namespace Cards56Web
             app.UseDefaultFiles();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseStaticFiles(new StaticFileOptions    //For the '.well-known' folder
-                {
-                    FileProvider = new PhysicalFileProvider(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/.well-known")),
-                    RequestPath = "/.well-known",
-                    ServeUnknownFileTypes = true,
-                });            
+            // app.UseStaticFiles(new StaticFileOptions    //For the '.well-known' folder
+            //     {
+            //         FileProvider = new PhysicalFileProvider(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/.well-known")),
+            //         RequestPath = "/.well-known",
+            //         ServeUnknownFileTypes = true,
+            //     });            
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
