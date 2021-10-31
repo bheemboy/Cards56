d8a3ce4147f8ef11a4484793dce70fe879957a09
Sunil Rehman <sunil.rehman@gmail.com>
2021-10-30 22:42:08 -0700

    fixed renew path

--- scripts/5.3.renew-56cards.net-cert.sh
+++ scripts/5.3.renew-56cards.net-cert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 /usr/bin/certbot renew -q --post-hook "/snap/bin/docker exec -it cards56web service nginx reload" \
-                       --work-dir /home/rehman/docker/cards56web/letsencrypt/work \
-                       --config-dir /home/rehman/docker/cards56web/letsencrypt/config \
-                       --logs-dir /home/rehman/docker/cards56web/letsencrypt/logs
+                       --work-dir /home/rehman/docker/ssl/letsencrypt/work \
+                       --config-dir /home/rehman/docker/ssl/letsencrypt/config \
+                       --logs-dir /home/rehman/docker/ssl/letsencrypt/logs
 
 # cron-job
 # 0 6 * * thu /home/rehman/docker/5.3.renew-56cards.net-cert.sh
