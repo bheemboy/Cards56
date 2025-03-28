921ed04a8dde537d3a742ac633b1102b4c5e3e17
Sunil Rehman <sunil.rehman@gmail.com>
2025-03-28 14:41:09 -0700

    updated for new oracle setup

--- oracle.56cards.com/ssh_connect.bat
+++ oracle.56cards.com/ssh_connect.bat
@@ -1,3 +1,5 @@
 @echo off
-ssh -i C:\Users\surehman\Nextcloud\AppConfigs\OracleCloud\56cards2\ssh-key-2025-03-17.key ubuntu@146.235.236.26
+ssh -i C:\Users\surehman\Nextcloud\AppConfigs\OracleCloud\MyInstance-1\ssh\myinstance-1.key ubuntu@129.146.65.5
+scp -i C:\Users\surehman\Nextcloud\AppConfigs\OracleCloud\MyInstance-1\ssh\myinstance-1.key C:\Projects\Cards56\oracle.56cards.com\docker-compose.yml ubuntu@129.146.65.5:/home/ubuntu
+
 pause
